e707ecf032f042899563d919a3670a16f05c2323
Mitchell Hedges <94mitchell94@gmail.com>
2020-05-27 10:46:02 +1000

    Splash screen update - works on a Oneplus 5T (Android). Need to change logos eventually but works for now

---
+++ RiECalmingPlan.Android/SplashActivity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace RiECalmingPlan.Droid {
+    [Activity(Label = "SplashActivity", Icon = "@mipmap/icon", Theme = "@style/SplashTheme", MainLauncher = true)]
+    // change icon = to change the app icon
+    public class SplashActivity : Activity {
+        // activity displays a splash image while app loadup, before loading the main activity
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            var intent = new Intent(this, typeof(MainActivity));
+            intent.AddFlags(ActivityFlags.ClearTop);
+            intent.AddFlags(ActivityFlags.SingleTop);
+            StartActivity(intent);
+            Finish();
+        }
+    }
+}
