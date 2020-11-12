1f5bcb109fb6384a254c66229759fcdf446c600a
Mitchell Hedges <94mitchell94@gmail.com>
2020-11-13 09:50:38 +1100

    Small change to distress tracker 'if' statement, and enabling feedback for questions other than steppers.

--- RiECalmingPlan/Pages/Page_DistressTracker.xaml.cs
+++ RiECalmingPlan/Pages/Page_DistressTracker.xaml.cs
@@ -24,11 +24,11 @@ namespace RiECalmingPlan.Pages {
         public void LevelButtonPressed(object sender, EventArgs e) {
             if (Label_DistressLevel.Text.ToString() == "Distress Level: Acute")
                 Frame_SupportPlan.BackgroundColor = (Color)Application.Current.Resources["Distress_Red"];
-            if (Label_DistressLevel.Text.ToString() == "Distress Level: Moderate")
+            else if (Label_DistressLevel.Text.ToString() == "Distress Level: Moderate")
                 Frame_SupportPlan.BackgroundColor = (Color)Application.Current.Resources["Distress_Orange"];
-            if (Label_DistressLevel.Text.ToString() == "Distress Level: Mild")
+            else if(Label_DistressLevel.Text.ToString() == "Distress Level: Mild")
                 Frame_SupportPlan.BackgroundColor = (Color)Application.Current.Resources["Distress_Yellow"];
-            if (Label_DistressLevel.Text.ToString() == "Distress Level: Calm")
+            else if(Label_DistressLevel.Text.ToString() == "Distress Level: Calm")
                 Frame_SupportPlan.BackgroundColor = (Color)Application.Current.Resources["Distress_Green"];
             Carousel.Position = 1;
         }
